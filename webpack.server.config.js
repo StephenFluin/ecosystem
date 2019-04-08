4e8f35eceeeffd77982de9e831fb5f8fe4f828d7
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2019-04-08 21:06:25 +0200

    add live reloading
    * add live reloading
    * update packages
    * Delete package-lock.json

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -5,7 +5,4 @@ const WebpackConfigFactory = require('@nestjs/ng-universal')
 module.exports = WebpackConfigFactory.create(webpack, {
   // Nest server for SSR
   server: './server/main.ts',
-  target: 'node'
 });
-
-console.log('is this even on?');
