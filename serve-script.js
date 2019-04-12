2a7382cad4fbd18c79da7013604d65a3e5e28d3b
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-04-12 14:08:09 +0200

    feature(): use live reload compiler (ng-universal)

--- serve-script.js
+++ serve-script.js
@@ -1,44 +1,6 @@
-const join = require('path').join;
-const concurrently = require('concurrently');
-const spawn = require('cross-spawn');
+const { LiveReloadCompiler } = require('@nestjs/ng-universal');
 
-const PROJECT_NAME = 'ecosystem';
-
-// Pre-build all packages (SSR)
-const SSR_BUILD_SCRIPT = `ng build --prod && ng run ${PROJECT_NAME}:server:production`;
-const SERVER_COMPILE_SCRIPT =
-  './node_modules/.bin/webpack --config webpack.server.config.js --progress --colors';
-
-const script = spawn(`${SSR_BUILD_SCRIPT} && ${SERVER_COMPILE_SCRIPT}`, {
-  shell: true,
-  stdio: 'inherit',
+const compiler = new LiveReloadCompiler({
+  projectName: 'ecosystem'
 });
-
-// Setup live reload server (websocket)
-const livereload = require('livereload');
-const server = livereload.createServer();
-server.watch(join(__dirname, 'dist', 'browser'));
-process.on('SIGINT', () => {
-  try {
-    server && server.close();
-  } catch {}
-  process.exit();
-});
-
-script.on('exit', code => {
-  if (code !== 0) {
-    return;
-  }
-  // Static server + live browser reload + watch mode + initial SSR (first build)
-  concurrently(
-    [
-      'ng build --aot --watch --delete-output-path=false',
-      'tsc --watch -p server/tsconfig.json',
-      'nodemon --watch dist dist/server-app/main --delay 1 --exec "node"',
-    ],
-    {
-      raw: true,
-      killOthers: true,
-    },
-  );
-});
+compiler.run();
